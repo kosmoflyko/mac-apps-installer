0f3ebecf5046c6ce70026ae7bc6a15ef8e2ef18e
Vladislav Gasanov <v.gasanov@kosmocourses.ru>
2021-06-06 12:07:45 +0300

    Add description

--- lib/brew-install-apps.sh
+++ lib/brew-install-apps.sh
@@ -1,99 +1,138 @@
 #!/bin/zsh
 
 # Browsers
-brew install firefox google-chrome
+brew install firefox                        # GUI: FireFox web browser
+brew install google-chrome                  # GUI: Chrome web browser
 
 # Golang
-brew install go
+brew install go                             # CLI: Golang
 
 # NodeJS
-brew install node yarn webpack
+brew install node                           # CLI: NodeJS (server JS)
+brew install yarn                           # CLI: JS packet manager
+brew install webpack                        # CLI: Web mobules builder
 
 # Python
-brew install pyenv pipenv
+brew install pyenv                          # CLI: Simple switch Python version
+brew install pipenv                         # CLI: Packet env manager
 
 # Haxe
-#brew install haxe neko
+#brew install haxe                          # CLI Haxe lang
+#brew install neko                          # CLI Standard Haxe VM
 
 # .Net
-#brew install mono-libgdiplus
-#brew install --cask dotnet-sdk
+#brew install mono-libgdiplus               # ---: libs for correct works .Net
+#brew install --cask dotnet-sdk             # CLI: .Net SDK (C# lang)
 
 # Test tool
-brew install jmeter postman charles wireshark
+brew install jmeter                         # GUI: Perfomance HTTP test tool
+brew install postman                        # GUI: Test tool for make HTTP request
+brew install charles                        # GUI: Proxy for dump and analyze HTTP packets
+brew install wireshark                      # GUI: Network tool for dump and analyze packets
 
 # Virtualization tools
-brew install xhyve vagrant virtualbox virtualbox-extension-pack
+brew install --HEAD xhyve                   # CLI: Free lightweight hypervizor base on bhyve
+brew install vagrant                        # CLI: Manager for VM and public cloud
+brew install virtualbox                     # GUI: Free hypervizor VirtualBox
+brew install virtualbox-extension-pack      # ---: Extension pack for VirtualBox
 
 # Containers
-brew install docker docker-compose docker-completion docker-compose-completion helm
+brew install docker                         # CLI: Docker container
+brew install docker-compose                 # CLI: Docker compose (simple packet manager for docker)
+brew install docker-completion              # ---: Shell completion for Docker
+brew install docker-compose-completion      # ---: Shell completion for Docker compose
+brew install helm                           # CLI: Packet manager for K8s
 
 # K8s
-brew install minikube
+brew install minikube                       # CLI: Dev env for K8s
 
 # OpenShift
-#brew install --cask minishift
+#brew install --cask minishift              # CLI: Dev env for OpenShift
 
 # IDE and dev tools
-brew install visual-studio-code maven-completion scenebuilder jetbrains-toolbox
+brew install visual-studio-code             # GUI: VS Code IDE
+brew install maven-completion               # ---: Shell completion for Maven
+brew install scenebuilder                   # GUI: Tool for make Java FX UI
+brew install jetbrains-toolbox              # GUI: Tool for manage JetBrains IDE 
 
 # PG Admin
 # (Comment because I use DBeaver and DataGrip)
-# brew install pgadmin4 
+#brew install pgadmin4                      # GUI: DB manager (Postgres)
 
 # Redis Manager
-brew install another-redis-desktop-manager
+brew install another-redis-desktop-manager  # GUI: Redis manager
 
 # DBeaver
-brew install --cask dbeaver-community
+brew install --cask dbeaver-community       # GUI: DB manager (Postgres, Cassandra, Ignite, ...)
 # requires Java8+:
-# brew install --cask adoptopenjdk
+#brew install --cask adoptopenjdk           # CLI: Last Open JDK
 
 # BPM develop (Camunda)
-brew install camunda-modeler
+brew install camunda-modeler                # GUI: Tool for make Camunda BPM scripts
 
 # Messangers
-brew install microsoft-teams skype zoom
+#brew install microsoft-teams 
+brew install skype                          # GUI: Skype messanger
+brew install zoom                           # GUI: Zoom videoconf
 
 # Office apps
-brew install onlyoffice
+brew install onlyoffice                     # GUI: Free ofiice pkg
 
 # Draw apps & tools
-brew install krita graphviz drawio inkscape gimp sketch
+brew install krita                          # GUI: Draw picture
+brew install gimp                           # GUI: Photo editor
+brew install inkscape                       # GUI: Vector graphics
+brew install sketch                         # GUI: Make UI for App and web
+brew install drawio                         # GUI: Diagram editor
+brew install graphviz                       # ---: Libs for make ULM diagram
 
 # Game
-brew install steam
+#brew install steam                         # GUI: Steam :)
 
 # Libs
-brew install libusb
+brew install libusb                         # ---: Lib for access USB
 
 # Radio tools
-brew install --cask arduino
-brew install --cask kicad
+brew install --cask arduino                 # GUI: Ardiuno IDE
+brew install --cask kicad                   # GUI: KiCAD make electronic devices
 
 # Media
-brew install vlc obs
+brew install vlc                            # GUI: VLC player
+brew install obs                            # GUI: OBS (mediastreaming tool)
 
 # System tool
-#brew install paragon-ntfs
-#brew install coconutbattery #Only for notebook
-brew install macs-fan-control rectangle iterm2 cleanmymac
-#brew install lunar
+#brew install paragon-ntfs                  # ---: Driver for R/W NTFS
+#brew install coconutbattery                # GUI: Monitoring battery on your Mac
+brew install macs-fan-control               # GUI: Control fan on your Mac
+#brew install rectangle                     # ---: Split you display
+brew install iterm2                         # GUI: The best terminal
+brew install cleanmymac                     # GUI: Clean your Mac
+#brew install lunar                         # GUI: Calibrate monitor ligthning
 
 # System CLI tools
-brew install htop jq mas screen
+brew install htop                           # CLI: Show process list
+brew install jq                             # CLI: JSON parser
+brew install mas                            # CLI: Install Apps from Apple Store
+brew install screen                         # CLI: Switch terminals without break it
 
 # Disk tool 
-brew install diskmaker-x balenaetcher unetbootin smartmontools disk-drill
-#brew install drivedx
+#brew install diskmaker-x                   # GUI: Make boot Mac OS disk for release before Big Sur (not compatible with Big Sur)
+brew install balenaetcher                   # GUI: Make boot USB/SD card
+brew install unetbootin                     # GUI: Make boot USB
+brew install smartmontools                  # CLI: Get SMART data of your disk
+brew install disk-drill                     # GUI: Repair information from disk
+#brew install drivedx                       # GUI: Monitoring your disk
 
 # Network tool
-brew install openvpn-connect wget ssh-copy-id telnet httpie netcat nmap bind
+brew install openvpn-connect                # CLI: OpenVpn client
+brew install wget                           # CLI: download tool
+brew install ssh-copy-id                    # CLI: Coppy public key to remote host
+brew install telnet                         # CLI: Telnet client
+brew install httpie                         # CLI: HTTP client
+brew install netcat                         # CLI: Network utility
+brew install nmap                           # CLI: Network scaner
+brew install bind                           # CLI: DNS check tools
 
 # Bechmark tool
-brew install heaven intel-power-gadget
-
-
-
-
-
+#brew install heaven                        # GUI: 3D benchmark
+#brew install intel-power-gadget            # GUI: Intel CPU base power monitoring
--- lib/sdk-install-tools.sh
+++ lib/sdk-install-tools.sh
@@ -1,5 +1,14 @@
 #!/bin/zsh
 
+# Language
+sdk install java 15.0.2.fx-librca
+sdk install java 11.0.10.fx-librca
+sdk install java 8.0.282.fx-librca
+sdk install java 21.0.0.2.r11-grl
+sdk use java 15.0.2.fx-librca
+sdk install groovy
+sdk install kotlin
+
 # Debug tool
 sdk install btrace
 sdk install visualvm
@@ -11,22 +20,13 @@ sdk install gaiden
 sdk install gradle
 sdk install gradleprofiler
 sdk install maven
-# sdk install mvnd
-# sdk install pomchecker
-sdk install lazybones
-# sdk install infrastructor
-
-# Language
-sdk install groovy
-sdk install kotlin
-sdk install java 15.0.2.fx-librca
-sdk install java 11.0.10.fx-librca
-sdk install java 8.0.282.fx-librca
-sdk install java 21.0.0.2.r11-grl
-sdk use java 15.0.2.fx-librca
+#sdk install mvnd
+#sdk install pomchecker
+#sdk install lazybones
+#sdk install infrastructor
 
 # Frameworks CLI tool
-# sdk install http4k
+#sdk install http4k
 sdk install micronaut
 sdk install springboot
 sdk install vertx
